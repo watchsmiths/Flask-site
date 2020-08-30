fd31ed5646af1d9c178ec7cacc0106ea33785e12
AX99 <rapha.mensah@gmail.com>
2020-08-30 12:59:41 +0100

    forms

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from .config import Config
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+app.config.from_object(Config)
 
 from app import routes 
--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators, Form, TextAreaField
+from validators import DataRequired
 
 # class RegistrationForm(Form):
 #     firstname = StringField('First Name', [validators.Length(min=4, max=25)])
@@ -28,12 +29,8 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 #     confirm = PasswordField('Repeat Password')
 
 
-# class Sellwatch(Form):
-#     firstname = StringField('First Name', [validators.Length(min=4, max=25)])
-#     lastname = StringField('Last Name', [validators.Length(min=4, max=25)])
-#     email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
-#     password = PasswordField('New Password', [
-#         validators.DataRequired(),
-#         validators.EqualTo('confirm', message='Passwords must match')
-#     ])
-#     confirm = PasswordField('Repeat Password')
+class Sellwatch(Form):
+    firstname = StringField('First Name', [validators.Length(min=4, max=25)])
+    lastname = StringField('Last Name', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35), validators.Email()])
+ 
