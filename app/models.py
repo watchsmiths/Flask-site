13724704130efc93382af51ddba25d9a088bbd2b
AX99 <rapha.mensah@gmail.com>
2020-09-16 07:02:34 +0100

    Changes

--- app/models.py
+++ app/models.py
@@ -1,19 +1,4 @@
-from app import db
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     firstname = db.Column(db.String(80), unique=False, nullable=False)
-#     lastname = db.Column(db.String(80), unique=False, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#     password = db.Column(db.String(180), unique=False, nullable=False)
-#     profile = db.Column(db.String(180), unique=False, nullable=False, default='profile.jpg')
-
-#     def __repr__(self):
-#         return '<User %r>' % self.firstname
-
-
-# class sellwatch(db.Model):
-#     # id = db.Column(db.Integer)
+from app import db  
 
 class Brands(db.Model):
     id = db.Column(db.Integer, primary_key=True)
